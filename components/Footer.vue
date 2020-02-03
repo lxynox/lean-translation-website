d6c0ba971f967946e54a9843479e2ca4af1ee874
lxynox <lxynox@gmail.com>
2020-02-03 19:15:05 +0800

    initial commit

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="flex justify-center bg-gray-200 py-4">ⓒ2020</footer>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <nav class="nav mx-4 my-2">
+      <div>Hello</div>
+      <div :style="{ margin: 'auto' }"></div>
+      <ul class="menu">
+        <li v-for="item in items" :key="item">
+          <nuxt-link
+            :to="'/' + item"
+            class="px-2 text-gray-500 hover:text-gray-700"
+          >
+            {{ item }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['items']
+}
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+  align-items: baseline;
+}
+
+.menu {
+  display: flex;
+}
+</style>
